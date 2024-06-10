d1f70056d4420e04d631f5703b5e0b5e88fb7ccb
davidboll <david.boll@me.com>
2024-06-11 01:29:23 +0200

    Adds server-port in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // binds to 0.0.0.0 instead of localhost
+    port: 5173, // make sure this is the port Render is scanning for
+  },
+});
